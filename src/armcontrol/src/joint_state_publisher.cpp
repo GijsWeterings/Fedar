516523e63a4f749d165f0abe6387d8c502cf031b
GijsWeterings <g.weterings@student.tudelft.nl>
2016-01-26 15:46:10 +0100

    arm uses moveit for motion

--- src/armcontrol/src/joint_state_publisher.cpp
+++ src/armcontrol/src/joint_state_publisher.cpp
@@ -4,8 +4,6 @@
 #include <trajectory_msgs/JointTrajectory.h>
 #include <tf/transform_broadcaster.h>
 #include <sensor_msgs/JointState.h>
-#include <threemxl/dxlassert.h>
-
 
 C3mxlROS* motor_upper;
 C3mxlROS* motor_lower;
@@ -22,63 +20,20 @@ void getJointVelocities(std::vector<double>& vels);
  * @param  id The 3mxl bus ID of the corresponding motor.
  * @return    Pointer to a C3mxlROS object, used to control the motor.
  */
-C3mxlROS* initializeMotor(int id, int direction){
+C3mxlROS* initializeMotor(int id){
     // Initialize 3mxl node and set config
     C3mxlROS *motor = new C3mxlROS("/threemxl_com");
     CDxlConfig *config  = new CDxlConfig();
     config->setID(id);
     motor->setConfig(config);
     motor->init(false);
-    DXL_SAFE_CALL(motor->set3MxlMode(EXTERNAL_INIT));
-    DXL_SAFE_CALL(motor->setAcceleration(0.5));
-    DXL_SAFE_CALL(motor->setSpeed(-0.1));
-    DXL_SAFE_CALL(motor->setTorque(0.1));
-    // usleep(10000);
-    // motor->set3MxlMode(POSITION_MODE);
-    // motor->setPos(0.3, 0.1);
 
     return motor;
-}
-
-void executeTrajectory(trajectory_msgs::JointTrajectory msg) {
-    ROS_INFO("%s", "Executing trajectory[]...");
-    updateMotor();
-
-    // if(!(msg.joint_names[0] == "upper_arm" && msg.joint_names[1] == "lower_arm")) {
-    //     ROS_ERROR("%s", msg.joint_names[0]);
-    //     ROS_ERROR("%s", "[ARM] We're dealing with different motors, abort!");
-    //     return;
-    // }
-
-    std_msgs::Header header = msg.header;
-    // Wait for starting point header.stamp
-    while(ros::Time::now() < header.stamp) {
-        usleep(100);
-    }
-
-    ros::Time start = ros::Time::now();
-    int32_t size = msg.points.size();
-    ROS_INFO("%d", size);
-    // ROS_INFO("%d", msg.points[0]);
-    for(int32_t i = 0; i < size; i++) {
-        ROS_INFO("%s", "Running next point");
-        trajectory_msgs::JointTrajectoryPoint this_point = msg.points[i];
-
-        motor_lower->setPos(this_point.positions[0], this_point.velocities[0], this_point.accelerations[0]);
-        motor_upper->setPos(this_point.positions[1], this_point.velocities[1], this_point.accelerations[1]);
-
-        updateMotor();
-        while(ros::Time::now() - start < this_point.time_from_start) {
-            usleep(100);
-        }
-    }
 }
 
 void updateMotor() {
     motor_upper->getPos();
     motor_lower->getPos();
-    ROS_INFO("%s", "Received pos...");
-
 }
 
 void getJointNames(std::vector<std::string>& names) {
@@ -124,22 +79,20 @@ void jointPublish() {
 
 int main(int argc, char **argv) {
     // Initialise node
-    ros::init(argc, argv, "armcontrol");
+    ros::init(argc, argv, "joint_state_publisher");
     // Initialise Nodehandler
     ros::NodeHandle nh;
-
-    ROS_INFO("LAUNCH");
 
     // Refresh rate
     ros::Rate loop_rate(30);
 
-    motor_upper  = initializeMotor(108, -1);
-    //motor_lower  = initializeMotor(109, 1);
+    motor_upper  = initializeMotor(108);
+    motor_lower  = initializeMotor(109);
 
-    // Subscriber
-    ros::Subscriber sub = nh.subscribe("command", 10, executeTrajectory);
+    ros::Publisher jointPublisher = nh.advertise<sensor_msgs::JointState>("joint_states", 100);
 
     while(ros::ok()) {
+        jointPublish();
         ros::spinOnce();
         loop_rate.sleep();
     }
